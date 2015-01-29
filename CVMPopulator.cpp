5bf211af87100ec73a177596efc8eb3e53ddedcf
stormeus <storm3us@gmail.com>
2015-01-28 21:08:28 -0500

    Write a CVMPopulator skeleton to get everything to compile

---
+++ CVMPopulator.cpp
@@ -0,0 +1,42 @@
+/*
+   Project: Squirrel Plugin for Vice City Multiplayer (0.4)
+   File: CVMPopulator.cpp
+
+   Copyright (c) 2015 Nelson Gomez (Stormeus)
+
+   Licensed under the MIT License. A copy of the License is available in
+   LICENSE.txt or at:
+
+       http://opensource.org/licenses/MIT
+
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+   THE SOFTWARE.
+*/
+
+#include "CVMPopulator.h"
+#include <sqrat.h>
+
+void CVMPopulator::Populate(HSQUIRRELVM v) {
+
+}
+
+void CVMPopulator::RegisterFunctions(HSQUIRRELVM v) {
+
+}
+
+void CVMPopulator::RegisterClasses(HSQUIRRELVM v) {
+
+}
+
+void CVMPopulator::RegisterConstants(HSQUIRRELVM v) {
+
+}
+
+void CVMPopulator::RegisterStructs(HSQUIRRELVM v) {
+
+}
