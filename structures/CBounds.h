7389d2f6211a086b3a41a4a4a9e19faf6bce4310
stormeus <stormeus@liberty-unleashed.co.uk>
2015-02-18 17:42:50 +0000

    Implement CBounds

--- structures/CBounds.h
+++ structures/CBounds.h
@@ -19,3 +19,57 @@
 */
 
 #pragma once
+#include <sqrat.h>
+#include "../Main.h"
+
+class CBounds
+{
+	public:
+		CBounds( float maxX, float minX, float maxY, float minY )
+		{
+			this->maxX = maxX;
+			this->minX = minX;
+			this->maxY = maxY;
+			this->minY = minY;
+		}
+
+		CBounds( int maxX, int minX, int maxY, int minY )
+		{
+			this->maxX = maxX;
+			this->minX = minX;
+			this->maxY = maxY;
+			this->minY = minY;
+		}
+
+		CBounds()
+		{
+			this->maxX = 0.0f;
+			this->minX = 0.0f;
+			this->maxY = 0.0f;
+			this->minY = 0.0f;
+		}
+
+		float maxX;
+		float minX;
+		float maxY;
+		float minY;
+
+		const std::string ToString() {
+			std::basic_stringstream<SQChar> out;
+			out << _SC("(") << maxX << _SC(",") << minX << _SC(",") << maxY << _SC(",") << minY << _SC(")");
+
+			return out.str();
+		}
+
+		static void Register(HSQUIRRELVM v) {
+			Sqrat::Class<CBounds> c(v, Sqrat::string("Bounds"));
+			c
+				.Var(_SC("MaxX"), &CBounds::maxX)
+				.Var(_SC("MinX"), &CBounds::minX)
+				.Var(_SC("MaxY"), &CBounds::maxY)
+				.Var(_SC("MinY"), &CBounds::minY)
+				.Func(_SC("_tostring"), &CBounds::ToString);
+
+			Sqrat::RootTable(v).Bind(_SC("Bounds"), c);
+		}
+};
