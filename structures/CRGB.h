5bc0a59ef06fcc68d747aec41529e8f47de6e8b6
stormeus <stormeus@liberty-unleashed.co.uk>
2015-02-18 18:43:01 +0000

    Implement CRGB

--- structures/CRGB.h
+++ structures/CRGB.h
@@ -19,3 +19,60 @@
 */
 
 #pragma once
+#include <sqrat.h>
+#include "../Main.h"
+
+class CRGB
+{
+	public:
+		typedef void (*RGBSetCallback)(CRGB* pRGB);
+		RGBSetCallback* m_pCallback = NULL;
+
+		CRGB( int r, int g, int b ) { this->r = r; this->g = g; this->b = b; }
+		CRGB() { this->r = 0; this->g = 0; this->b = 0; }
+
+		void SetCallbck(RGBSetCallback* pCallback) { m_pCallback = pCallback; }
+		void FreeCallback() { m_pCallback = NULL; }
+
+		void SetR(int r) { this->r = r; ProcessCallback(); }
+		void SetG(int g) { this->g = g; ProcessCallback(); }
+		void SetB(int b) { this->b = b; ProcessCallback(); }
+
+		int GetR() { return r; }
+		int GetG() { return g; }
+		int GetB() { return b; }
+
+		unsigned int ToInt() {
+			return this->r << 16 | this->g << 8 | this->b;
+		}
+
+		const std::string ToString() {
+			std::basic_stringstream<SQChar> out;
+			out << _SC("(") << r << _SC(",") << g << _SC(",") << b << _SC(")");
+
+			return out.str();
+		}
+
+		static void Register(HSQUIRRELVM v) {
+			Sqrat::Class<CRGB> c(v, Sqrat::string("RGB"));
+			c
+				.Prop(_SC("r"), &CRGB::GetR, &CRGB::GetR)
+				.Prop(_SC("g"), &CRGB::GetG, &CRGB::GetG)
+				.Prop(_SC("b"), &CRGB::GetB, &CRGB::GetB)
+				.Func(_SC("ToInt"), &CRGB::ToInt)
+				.Func(_SC("_tostring"), &CRGB::ToString);
+
+			Sqrat::RootTable(v).Bind(_SC("RGB"), c);
+		}
+
+	private:
+		void ProcessCallback() {
+			if (m_pCallback) {
+				(*m_pCallback)(this);
+			}
+		}
+
+		int r;
+		int g;
+		int b;
+};
