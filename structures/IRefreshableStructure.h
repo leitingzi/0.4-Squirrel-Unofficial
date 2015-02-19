c1961b4f5b874edddcbaf2291be6c76d9926dedd
stormeus <stormeus@liberty-unleashed.co.uk>
2015-02-19 17:26:03 +0000

    Add an interface for "refreshable" structure with setter callbacks

---
+++ structures/IRefreshableStructure.h
@@ -0,0 +1,43 @@
+/*
+   Project: Squirrel Plugin for Vice City Multiplayer (0.4)
+   File: IRefreshableStructure.h
+
+   Copyright (c) 2015 Nelson Gomez (Stormeus)
+
+   Licensed under the MIT License. A copy of the License is available in
+   LICENSE or at:
+
+       http://opensource.org/licenses/MIT
+
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+   THE SOFTWARE.
+*/
+
+#pragma once
+#include <stdlib.h>
+
+template<typename T>
+class IRefreshableStructure
+{
+	public:
+		typedef void (*StructureSetCallback)(T* pQuat);
+		StructureSetCallback* m_pCallback = NULL;
+
+		void SetCallback(StructureSetCallback* pCallback) {
+			m_pCallback = pCallback;
+		}
+
+		void FreeCallback(void) { m_pCallback = NULL; }
+
+	protected:
+		void ProcessCallback(T* pStruct) {
+			if (m_pCallback) {
+				(*m_pCallback)(pStruct);
+			}
+		}
+};
